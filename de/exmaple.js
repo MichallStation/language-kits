8d1184d48ed40bdd6b08a538850388194279074c
ltndat <ltndat@outlook.com>
2024-12-10 23:21:26 -0500

    make it better

--- de/exmaple.js
+++ de/exmaple.js
@@ -1,11 +1,4 @@
-// const wordPairs = require('./WordPairs.json');
-const fs = require('fs');
-const path = require('path');
-const wordPairs = require('./WordPairs copy.json');
-const added = require('./added.json');
-
-const re = wordPairs.filter(
-  ({ word1, word2 }) => word1.toLowerCase() !== word2.toLocaleLowerCase()
-);
-// console.log(re.length);
-// fs.writeFileSync('./WordPairs copy.json', JSON.stringify(re));
+const re = require('./SpellingRules.json');
+console.log(re.consonants.length);
+console.log(re.vowels.length);
+console.log(re.compound.length);
