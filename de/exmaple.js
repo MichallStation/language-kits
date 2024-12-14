f831ccce574aa25906e0a8c79ef4b9108fb3cda5
ltndat <ltndat@outlook.com>
2024-12-14 00:07:28 -0500

    make it better

--- de/exmaple.js
+++ de/exmaple.js
@@ -2,3 +2,13 @@ const re = require('./SpellingRules.json');
 console.log(re.consonants.length);
 console.log(re.vowels.length);
 console.log(re.compound.length);
+
+const vowels = require('./Vowels.json').map(({ letter, ipa }) => {
+  return { letter, ipa };
+});
+
+vowels.push({
+  letter: 'ER',
+  ipa: 'ɐ',
+});
+console.log(JSON.stringify(vowels));
